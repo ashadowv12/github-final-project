94b4d32d441593e6b3e6aeabdac96f3501953035
ashadowv12 <ashadowv10@gmail.com>
2025-05-15 11:08:45 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,7 @@
-
+echo "# simple-interest.sh" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ashadowv12/github-final-project/edit/main/simple-interest.sh
+git push -u origin main
